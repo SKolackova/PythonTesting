97d14cdb5931032e0ca0826f2a8e878d48da8dab
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-03-14 18:49:50 +0100

    Open web driver

--- UI/Login_UI_tests.py
+++ UI/Login_UI_tests.py
@@ -1,20 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-
-
 def Correct_login_test():
-    zadek
-
-
-
-
+    driver = open_web_page()
+    driver.get("https://testpages.herokuapp.com/styled/cookies/adminlogin.html")
+    driver.find_element_by_name('username').send_keys('Admin')
+    driver.find_element_by_name('password').send_keys('AdminPass')
+    driver.find_element_by_id('login').click()
+    porovnani = driver.find_element_by_id('gologin').text
+    assert "Go To Login" == porovnani
+    print("hovínko")
 
 def open_web_page():
-    driver = webdriver.Firefox()
-    driver.get("https://www.python.org")
-    print(driver.title)
+    return webdriver.Firefox(executable_path=r"C:\Users\sabin\PythonTesting\UI\geckodriver.exe")
 
-
-
-open_web_page()
+Correct_login_test()

7c736d78f648fa027202572170c589af55deb33d
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-03-14 17:53:51 +0100

    problém

---
+++ UI/Login_UI_tests.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+
+def Correct_login_test():
+    zadek
+
+
+
+
+
+def open_web_page():
+    driver = webdriver.Firefox()
+    driver.get("https://www.python.org")
+    print(driver.title)
+
+
+
+open_web_page()

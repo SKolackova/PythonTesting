dea2075a7e3ef99809bf73e65444a9910798f718
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-03-15 20:41:59 +0100

    Negativní přihlášení

--- UI/Login_UI_tests.py
+++ UI/Login_UI_tests.py
@@ -14,6 +14,18 @@ class TestCase(unittest.TestCase):
         self.assertEqual("Go To Login", porovnani) 
         driver.quit()
 
+    def Incorrect_password_login_test():
+        driver = open_web_page()
+        driver.get("https://testpages.herokuapp.com/styled/cookies/adminlogin.html")
+        driver.find_element_by_name('username').send_keys('Admin')
+        driver.find_element_by_name('password').send_keys('Spatneheslo')
+        driver.find_element_by_id('login').click()
+        porovnani = driver.find_element_by_id('gologin').text    # musím si ještě najít, jak udělat záporné porovnání
+        self.assertEqual("Go To Login", porovnani) 
+        driver.quit()
+
+
+
 def open_web_page():
         return webdriver.Firefox(executable_path=r"C:\Users\Jaroslav Kafka\uceni\PythonTesting\UI\geckodriver.exe")
     

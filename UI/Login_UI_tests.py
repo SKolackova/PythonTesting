aa2666f12524d7ccfd24ec8effe27f50f111e76a
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-03-16 21:12:12 +0100

    Přidání negativního testu-špatné heslo

--- UI/Login_UI_tests.py
+++ UI/Login_UI_tests.py
@@ -14,14 +14,14 @@ class TestCase(unittest.TestCase):
         self.assertEqual("Go To Login", porovnani) 
         driver.quit()
 
-    def Incorrect_password_login_test():
+    def Incorrect_password_login_test(self):
         driver = open_web_page()
         driver.get("https://testpages.herokuapp.com/styled/cookies/adminlogin.html")
         driver.find_element_by_name('username').send_keys('Admin')
         driver.find_element_by_name('password').send_keys('Spatneheslo')
         driver.find_element_by_id('login').click()
-        porovnani = driver.find_element_by_id('gologin').text    # musím si ještě najít, jak udělat záporné porovnání,test jestli funguje git
-        self.assertEqual("Go To Login", porovnani) 
+        porovnani = driver.find_element_by_class_name('loginmessage').text    # musím si ještě najít, jak udělat záporné porovnání
+        self.assertEqual("Login Details Incorrect", porovnani) 
         driver.quit()
 
 
